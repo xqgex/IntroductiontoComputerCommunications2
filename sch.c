d099dfdfe15d3dd2adbf88b4f53796e10abd09c1
xqgex <xqgex.n@gmail.com>
2017-05-29 23:00:24 +0300

    Add support for 'long' type variables

--- sch.c
+++ sch.c
@@ -11,20 +11,22 @@
 #define F_ERROR_FUNCTION_STRTOL_MSG	"[Error] strtol() failed with an error: %s\n"
 #define F_ERROR_INPUT_CLOSE_MSG		"[Error] Close input file: %s\n"
 #define F_ERROR_INPUT_FILE_MSG		"[Error] Input file '%s': %s\n"
-#define F_ERROR_IP_INVALID_MSG		"[Error] The ip address %s is not a valis IPv4\n"
-#define F_ERROR_LENGTH_INVALID_MSG	"[Error] The length must be a number between 64 to 16384\n"
+#define F_ERROR_IP_INVALID_MSG		"[Error] The ip address %s is not a valid IPv4\n"
+#define F_ERROR_LENGTH_INVALID_MSG	"[Error] The length '%s' is not a number between 64 to 16384\n"
 #define F_ERROR_OUTPUT_CLOSE_MSG	"[Error] Close output file: %s\n"
 #define F_ERROR_OUTPUT_FILE_MSG		"[Error] Output file '%s': %s\n"
-#define F_ERROR_QUANTUM_INVALID_MSG	"[Error] The quantum must be non negative integer\n"
-#define F_ERROR_PORT_INVALID_MSG	"[Error] The port must be a number between 0 to 65535\n"
+#define F_ERROR_QUANTUM_INVALID_MSG	"[Error] The quantum '%s' is not non negative integer\n"
+#define F_ERROR_PKTID_INVALID_MSG	"[Error] The packet ID '%s' is not a valid long integer\n"
+#define F_ERROR_PORT_INVALID_MSG	"[Error] The port '%s' is not a number between 0 to 65535\n"
+#define F_ERROR_TIME_INVALID_MSG	"[Error] The time '%s' is not a valid long integer\n"
 #define F_ERROR_TYPE_MSG		"[Error] The only valid types are RR and DRR\n"
-#define F_ERROR_WEIGHT_INVALID_MSG	"[Error] The weight must be a positive integer\n"
+#define F_ERROR_WEIGHT_INVALID_MSG	"[Error] The weight '%s' is not a positive integer\n"
 
 FILE* IN_FILE = NULL;			/* The input file */
 FILE* OUT_FILE = NULL;			/* The output file */
 typedef struct Packets {
-	char pktID[20];		/* Unique ID (long int [-9223372036854775808,9223372036854775807]) */
-	char Time[20];		/* Arrival time (long int [-9223372036854775808,9223372036854775807]) */
+	long pktID;		/* Unique ID (long int [-9223372036854775808,9223372036854775807]) */
+	long Time;		/* Arrival time (long int [-9223372036854775808,9223372036854775807]) */
 	char Sadd[15];		/* Source ip address (string Ex. '192.168.0.1') */
 	int Sport;		/* Source port (int [0,65535]) */
 	char Dadd[15];		/* Destination ip address (string Ex. '192.168.0.1') */
@@ -86,7 +88,7 @@ int validate_IPv4(const char *s) { /* http://stackoverflow.com/questions/791982/
 	}
 	return 0;
 }
-/* int convert_strin2int(char *input, int *output) { }
+/* int convert_strin2long(char *input, int *output) { }
  * 
  * Receive:	input string
  * 		pointer where to save the output int
@@ -96,46 +98,47 @@ int validate_IPv4(const char *s) { /* http://stackoverflow.com/questions/791982/
  * Return 0 if succeed,
  * Return errno if error occurred.
  */
-int convert_strin2int(char *input, int *output, int from, int to, char *error_string) {
+int convert_strin2long(char *input, long *output, long from, long to, char *error_string) { /* https://linux.die.net/man/3/strtol */
 	/* Function variables */
 	char errmsg[256];	/* The message to print in case of an error */
 	char output_char[10];	/* variable for sprintf() */
 	char *endptr;		/* variable for strtol() */
 	/* Function body */
 	*output = strtol(input, &endptr, 10); /* If an underflow occurs. strtol() returns LONG_MIN. If an overflow occurs, strtol() returns LONG_MAX. In both cases, errno is set to ERANGE. */
-	if ((errno == ERANGE && (*output == (int)LONG_MAX || *output == (int)LONG_MIN)) || (errno != 0 && *output == 0)) {
+	if ((errno == ERANGE && (*output == LONG_MAX || *output == LONG_MIN)) || (errno != 0 && *output == 0)) {
 		strerror_r(errno, errmsg, 255);
 		fprintf(stderr, F_ERROR_FUNCTION_STRTOL_MSG, errmsg);
 		return errno;
 	} else if ((endptr == input) || (*output < from) || (*output > to)) { /* (Empty string) or (Not in range) */
-		fprintf(stderr, error_string);
+		fprintf(stderr, error_string, input);
 		return EXIT_FAILURE;
-	} else if (sprintf(output_char, "%d", *output) < 0) { /* sprintf(), If an output error is encountered, a negative value is returned. */
+	} else if (sprintf(output_char, "%ld", *output) < 0) { /* sprintf(), If an output error is encountered, a negative value is returned. */
 		fprintf(stderr, F_ERROR_FUNCTION_SPRINTF_MSG);
 		return EXIT_FAILURE;
 	} else if (strncmp(output_char, input, 10) != 0) { /* Contain invalid chars */
-		fprintf(stderr, error_string);
+		fprintf(stderr, error_string, input);
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 }
-/* int read_packet() { }
+/* packet read_packet() { }
  * 
  * Receive nothing
  * Read one line from the input file and parse her
  * Return packet object if read succeed,
  * Return NULL in case EOF reached or if an error occurred.
  */
-int read_packet() {
+packet read_packet() {
 	/* Function variables */
 	char line[105];	/* 105 == pktID[20]+Time[20]+Sadd[15]+Sport[5]+Dadd[15]+Dport[5]+length[5]+weight[11]+spaces[7]+"\r\n"[2] */
 	char *word;	/* Each string splited by whitespace */
 	char *newline;
 	int count;	/* Count how many words was in the current line */
 	int res = 0;	/* Temporary variable to store function response */
+	long temp = 0;	/* Temporary variable */
 	/* Function body */
-	packet pk = malloc(sizeof(packet));
-	if (fgets(line, sizeof(line), IN_FILE)) { /* return s on success, and NULL on error or when end of file occurs while no characters have been read. */
+	packet pk = malloc(sizeof(struct Packets));
+	if ((fgets(line, sizeof(line), IN_FILE))&&(strlen(line) > 26)) { /* return s on success, and NULL on error or when end of file occurs while no characters have been read. */
 		count = 0; /* Init the counter */
 		word = strtok(line," ");
 		while (word != NULL) {
@@ -150,71 +153,92 @@ int read_packet() {
 			}
 			/* Parse the current word */
 			if ((count == 0)&&(strlen(word) <= 20)) {
-				strncpy(pk->pktID, word, 20);
+				if ((res = convert_strin2long(word, &temp, LONG_MIN, LONG_MAX, F_ERROR_PKTID_INVALID_MSG)) > 0) {
+					return NULL; /* Error occurred, Exit */
+				} else {
+					pk->pktID = temp;
+				}
 			} else if ((count == 1)&&(strlen(word) <= 20)) {
-				strncpy(pk->Time, word, 20);
+				if ((res = convert_strin2long(word, &temp, LONG_MIN, LONG_MAX, F_ERROR_TIME_INVALID_MSG)) > 0) {
+					return NULL; /* Error occurred, Exit */
+				} else {
+					pk->Time = temp;
+				}
 			} else if ((count == 2)&&(strlen(word) <= 15)) {
 				if (validate_IPv4(word) == -1) {
 					fprintf(stderr, F_ERROR_IP_INVALID_MSG, word);
-					return 0;
+					return NULL;
 				} else {
 					strncpy(pk->Sadd, word, 15);
 				}
 			} else if ((count == 3)&&(strlen(word) <= 5)) {
-				if ((res = convert_strin2int(word, &pk->Sport, 0, 65535, F_ERROR_PORT_INVALID_MSG)) > 0) {
-					return 0; /* Error occurred, Exit */
+				if ((res = convert_strin2long(word, &temp, 0, 65535, F_ERROR_PORT_INVALID_MSG)) > 0) {
+					return NULL; /* Error occurred, Exit */
+				} else {
+					pk->Sport = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 				}
 			} else if ((count == 4)&&(strlen(word) <= 15)) {
 				if (validate_IPv4(word) == -1) {
 					fprintf(stderr, F_ERROR_IP_INVALID_MSG, word);
-					return 0;
+					return NULL;
 				} else {
 					strncpy(pk->Dadd, word, 15);
 				}
 			} else if ((count == 5)&&(strlen(word) <= 5)) {
-				if ((res = convert_strin2int(word, &pk->Dport, 0, 65535, F_ERROR_PORT_INVALID_MSG)) > 0) {
-					return 0; /* Error occurred, Exit */
+				if ((res = convert_strin2long(word, &temp, 0, 65535, F_ERROR_PORT_INVALID_MSG)) > 0) {
+					return NULL; /* Error occurred, Exit */
+				} else {
+					pk->Dport = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 				}
 			} else if ((count == 6)&&(strlen(word) <= 5)) {
-				if ((res = convert_strin2int(word, &pk->length, 64, 16384, F_ERROR_LENGTH_INVALID_MSG)) > 0) {
-					return 0; /* Error occurred, Exit */
+				if ((res = convert_strin2long(word, &temp, 64, 16384, F_ERROR_LENGTH_INVALID_MSG)) > 0) {
+					return NULL; /* Error occurred, Exit */
+				} else {
+					pk->length = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 				}
 			} else if ((count == 7)&&(strlen(word) <= 11)) {
-				if ((res = convert_strin2int(word, &pk->weight, 1, 2147483647, F_ERROR_WEIGHT_INVALID_MSG)) > 0) {
-					return 0; /* Error occurred, Exit */
+				if ((res = convert_strin2long(word, &temp, 1, INT_MAX, F_ERROR_WEIGHT_INVALID_MSG)) > 0) {
+					return NULL; /* Error occurred, Exit */
+				} else {
+					pk->weight = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 				}
 			} else {
-				printf("Died at %d - %s\n", count, word); /* TODO DEBUG XXX XXX XXX */
-				/* free(pk); // Error in `./sch': free(): invalid next size (fast) *//* TODO DEBUG XXX XXX XXX */
-				return 0; /* Invalid input, Length is too long */
+				return NULL; /* Invalid input, Length is too long */
 			}
 			count += 1; /* Inc the counter */
 			word = strtok(NULL, " ");
 		}
 		if (count < 7) {
-			printf("Died with %d\n", count); /* TODO DEBUG XXX XXX XXX */
-			return 0; /* Invalid input, Length is too short */
+			return NULL; /* Invalid input, Length is too short */
 		}
-		printf("pktID='%s', Time='%s', Sadd='%s', Sport='%d', Dadd='%s', Dport='%d', length='%d', weight='%s'\n", pk->pktID, pk->Time, pk->Sadd, pk->Sport, pk->Dadd, pk->Dport, pk->length, pk->weight); /* TODO DEBUG XXX XXX XXX */
-		return 1;
+		printf("pktID='%ld', Time='%ld', Sadd='%s', Sport='%d', Dadd='%s', Dport='%d', length='%d', weight='%d'\n", pk->pktID, pk->Time, pk->Sadd, pk->Sport, pk->Dadd, pk->Dport, pk->length, pk->weight); /* TODO DEBUG XXX DELME XXX XXX */
+		return pk;
 	} else {
-		return 0; /* EOF */
+		return NULL; /* EOF */
 	}
 } 
-
-int send_packet() {
-	/* TODO */
-	return 1;
-}
-
+/* int send_packet() { }
+ * 
+ * Receive ??? XXX ??? XXX
+ * ??? XXX ??? XXX
+ * Return ??? XXX ??? XXX
+ */
+int send_packet() { /* TODO TODO TODO */
+	return 1; /* TODO TODO TODO */
+} /* TODO TODO TODO */
+/* int main(int argc, char *argv[]) { }
+ * 
+ * Receive command line arguments
+ * ??? XXX ??? XXX
+ * Return the program exit code
+ */
 int main(int argc, char *argv[]) {
 	/* Function variables */
-	char errmsg[256];			/* The message to print in case of an error */
-	int input_quantum = 0;			/* The quantum */
-	int input_weight = 0;			/* The weight */
-	int more_to_send = 1;			/* Flag for send loop */
-	int more_to_read  = 1;			/* Flag for acsept loop */
-	int res = 0;				/* Temporary variable to store function response */
+	char errmsg[256];		/* The message to print in case of an error */
+	int input_quantum = 0;		/* The quantum */
+	int input_weight = 0;		/* The weight */
+	int res = 0;			/* Temporary variable to store function response */
+	long temp = 0;			/* Temporary variable */
 	/* Check correct call structure */
 	if (argc != 6) {
 		if (argc < 6) {
@@ -243,18 +267,23 @@ int main(int argc, char *argv[]) {
 		return program_end(errno);
 	}
 	/* Check weight => argv[4] */
-	if ((res = convert_strin2int(argv[4], &input_weight, 1, 2147483647, F_ERROR_WEIGHT_INVALID_MSG)) > 0) {
+	if ((res = convert_strin2long(argv[4], &temp, 1, INT_MAX, F_ERROR_WEIGHT_INVALID_MSG)) > 0) {
 		return program_end(res); /* Error occurred, Exit */
+	} else {
+		input_weight = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 	}
 	/* Check quantum => argv[5] */
-	if ((res = convert_strin2int(argv[5], &input_quantum, 0, 2147483647, F_ERROR_QUANTUM_INVALID_MSG)) > 0) {
+	if ((res = convert_strin2long(argv[5], &temp, 0, INT_MAX, F_ERROR_QUANTUM_INVALID_MSG)) > 0) {
 		return program_end(res); /* Error occurred, Exit */
+	} else {
+		input_quantum = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 	}
 	/* TODO TODO TODO */
-	while (more_to_send && more_to_read) {
-		more_to_read = read_packet();
-		more_to_send = send_packet();
-	}
-	/* TODO TODO TODO */
+	input_quantum = input_weight; /* TODO XXX DELME XXX TODO */
+	input_weight = input_quantum; /* TODO XXX DELME XXX TODO */
+	while (read_packet()) { /* TODO TODO TODO */
+		send_packet(); /* TODO TODO TODO */
+	} /* TODO TODO TODO */
+	/* Exit */
 	return program_end(EXIT_SUCCESS);
 }
