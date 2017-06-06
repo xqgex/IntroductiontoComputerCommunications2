fd882d5d2ff14761bf77f376943284b71c1db913
xqgex <xqgex.n@gmail.com>
2017-06-06 21:28:40 +0300

    Fix pointers problems

--- sch.c
+++ sch.c
@@ -26,7 +26,7 @@
 typedef struct DataStructure {
 	struct Packets** head;	/* Pointer to the head of the round double linked list */
 	int count;		/* The total number of packets, Need to be updated in every insert & delete */
-} *structure; 
+} structure; 
 typedef struct Packets {
 	long pktID;		/* Unique ID (long int [-9223372036854775808,9223372036854775807]) */
 	long Time;		/* Arrival time (long int [-9223372036854775808,9223372036854775807]) */
@@ -41,11 +41,12 @@ typedef struct Packets {
 	struct Packets** prev;	/* Pointer to previously packet in the list */
 	struct Packets** up;	/* Pointer to upper packet in the queue */
 	struct Packets** down;	/* Pointer to lower packet in the queue */
-} *packet; 
+} packet; 
 FILE* IN_FILE = NULL;		/* The input file */
 FILE* OUT_FILE = NULL;		/* The output file */
 long CLOCK = LONG_MIN;		/* The current time */
-structure STRUCTURE;	/* Our data structure */
+structure STRUCTURE;		/* Our data structure */
+packet NILL;			/* NULL packet */
 
 /* int program_end(int error) { }
  *
@@ -133,14 +134,14 @@ int convert_strin2long(char *input, long *output, long from, long to, char *erro
 	}
 	return EXIT_SUCCESS;
 }
-/* packet read_packet(int* total_num) { }
+/* int read_packet(packet* pk, int* total_num) { }
  * 
- * Receive nothing
+ * Receive packet object and the default weight
  * Read one line from the input file and parse her
- * Return packet object if read succeed,
- * Return NULL in case EOF reached or if an error occurred.
+ * Return 1 if read succeed,
+ * Return 0 in case EOF reached or if an error occurred.
  */
-packet read_packet(int default_weight) {
+int read_packet(packet* pk, int default_weight) {
 	/* Function variables */
 	char line[105];	/* 105 == pktID[20]+Time[20]+Sadd[15]+Sport[5]+Dadd[15]+Dport[5]+length[5]+weight[11]+spaces[7]+"\r\n"[2] */
 	char *word;	/* Each string splited by whitespace */
@@ -149,7 +150,6 @@ packet read_packet(int default_weight) {
 	int res = 0;	/* Temporary variable to store function response */
 	long temp = 0;	/* Temporary variable */
 	/* Function body */
-	packet pk = malloc(sizeof(struct Packets));
 	if ((fgets(line, sizeof(line), IN_FILE))&&(strlen(line) > 26)) { /* return s on success, and NULL on error or when end of file occurs while no characters have been read. */
 		count = 0; /* Init the counter */
 		word = strtok(line," ");
@@ -166,64 +166,64 @@ packet read_packet(int default_weight) {
 			/* Parse the current word */
 			if ((count == 0)&&(strlen(word) <= 20)) {
 				if ((res = convert_strin2long(word, &temp, LONG_MIN, LONG_MAX, F_ERROR_PKTID_INVALID_MSG)) > 0) {
-					return NULL; /* Error occurred, Exit */
+					return 0; /* Error occurred, Exit */
 				} else {
 					pk->pktID = temp;
 				}
 			} else if ((count == 1)&&(strlen(word) <= 20)) {
 				if ((res = convert_strin2long(word, &temp, LONG_MIN, LONG_MAX, F_ERROR_TIME_INVALID_MSG)) > 0) {
-					return NULL; /* Error occurred, Exit */
+					return 0; /* Error occurred, Exit */
 				} else {
 					pk->Time = temp;
 				}
 			} else if ((count == 2)&&(strlen(word) <= 15)) {
 				if (validate_IPv4(word) == -1) {
 					fprintf(stderr, F_ERROR_IP_INVALID_MSG, word);
-					return NULL;
+					return 0;
 				} else {
 					strncpy(pk->Sadd, word, 15);
 				}
 			} else if ((count == 3)&&(strlen(word) <= 5)) {
 				if ((res = convert_strin2long(word, &temp, 0, 65535, F_ERROR_PORT_INVALID_MSG)) > 0) {
-					return NULL; /* Error occurred, Exit */
+					return 0; /* Error occurred, Exit */
 				} else {
 					pk->Sport = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 				}
 			} else if ((count == 4)&&(strlen(word) <= 15)) {
 				if (validate_IPv4(word) == -1) {
 					fprintf(stderr, F_ERROR_IP_INVALID_MSG, word);
-					return NULL;
+					return 0;
 				} else {
 					strncpy(pk->Dadd, word, 15);
 				}
 			} else if ((count == 5)&&(strlen(word) <= 5)) {
 				if ((res = convert_strin2long(word, &temp, 0, 65535, F_ERROR_PORT_INVALID_MSG)) > 0) {
-					return NULL; /* Error occurred, Exit */
+					return 0; /* Error occurred, Exit */
 				} else {
 					pk->Dport = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 				}
 			} else if ((count == 6)&&(strlen(word) <= 5)) {
 				if ((res = convert_strin2long(word, &temp, 1, 16384, F_ERROR_LENGTH_INVALID_MSG)) > 0) { /* TODO TODO TODO Change from 1 to 64 TODO TODO TODO !!!!!!!!!!!!! */
-					return NULL; /* Error occurred, Exit */
+					return 0; /* Error occurred, Exit */
 				} else {
 					pk->length = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 				}
 			} else if ((count == 7)&&(strlen(word) <= 11)) {
 				if ((res = convert_strin2long(word, &temp, 1, INT_MAX, F_ERROR_WEIGHT_INVALID_MSG)) > 0) {
-					return NULL; /* Error occurred, Exit */
+					return 0; /* Error occurred, Exit */
 				} else if (temp) { /* There was a value for weight in the input file */
 					pk->weight = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 				} else {
 					pk->weight = default_weight; /* Use the default weight */
 				}
 			} else {
-				return NULL; /* Invalid input, Length is too long */
+				return 0; /* Invalid input, Length is too long */
 			}
 			count += 1; /* Inc the counter */
 			word = strtok(NULL, " ");
 		}
 		if (count < 7) {
-			return NULL; /* Invalid input, Length is too short */
+			return 0; /* Invalid input, Length is too short */
 		}
 		printf("[A] pktID='%ld', Time='%ld', Sadd='%s', Sport='%d', Dadd='%s', Dport='%d', length='%d', weight='%d'\n", pk->pktID, pk->Time, pk->Sadd, pk->Sport, pk->Dadd, pk->Dport, pk->length, pk->weight); /* TODO DEBUG XXX DELME XXX XXX */
 		pk->start_byte = 0; /* From were should next transmition Start */
@@ -231,9 +231,9 @@ packet read_packet(int default_weight) {
 		pk->prev = NULL; /* Pointer to prev packet in the list */
 		pk->up = NULL; /* Pointer to upper packet in the queue */
 		pk->down = NULL; /* Pointer to lower packet in the queue */
-		return pk;
+		return 1;
 	} else {
-		return NULL; /* EOF */
+		return 0; /* EOF */
 	}
 } 
 /* int send_packet(packet pk) { }
@@ -252,19 +252,21 @@ int send_packet(packet pk) { /* TODO TODO TODO  Write to output file */
  * Copy the content of the source packet to the destination packet
  */
 void copy_packet(packet* src, packet* dst) {
-	(*dst)->pktID	= (*src)->pktID;
-	(*dst)->Time	= (*src)->Time;
-	strncpy((*dst)->Sadd,(*src)->Sadd,15);
-	(*dst)->Sport	= (*src)->Sport;
-	strncpy((*dst)->Dadd,(*src)->Dadd,15);
-	(*dst)->Dport	= (*src)->Dport;
-	(*dst)->length	= (*src)->length;
-	(*dst)->weight	= (*src)->weight;
-	(*dst)->start_byte	= (*src)->start_byte;
-	(*dst)->next	= (*src)->next;
-	(*dst)->prev	= (*src)->prev;
-	(*dst)->up	= (*src)->up;
-	(*dst)->down	= (*src)->down;
+	printf("~~~START copy_packet~~~\n"); /* XXX */
+	dst->pktID	= src->pktID;
+	dst->Time	= src->Time;
+	strncpy(dst->Sadd,src->Sadd,15);
+	dst->Sport	= src->Sport;
+	strncpy(dst->Dadd,src->Dadd,15);
+	dst->Dport	= src->Dport;
+	dst->length	= src->length;
+	dst->weight	= src->weight;
+	dst->start_byte	= src->start_byte;
+	dst->next	= src->next;
+	dst->prev	= src->prev;
+	dst->up	= src->up;
+	dst->down	= src->down;
+	printf("~~~END copy_packet~~~\n"); /* XXX */
 	return;
 }
 /* int same_flow(packet* pacA, packet* pacB) { }
@@ -275,64 +277,88 @@ void copy_packet(packet* src, packet* dst) {
  * Return 0 o.w
  */
 int same_flow(packet* pacA, packet* pacB) {
-	if ((strncmp((*pacA)->Sadd,(*pacB)->Sadd,15))&&
-	    (strncmp((*pacA)->Dadd,(*pacB)->Dadd,15))&&
-	    ((*pacA)->Sport == (*pacB)->Sport)&&
-	    ((*pacA)->Dport == (*pacB)->Dport)) {
+	printf("~~~START same_flow~~~\n"); /* XXX */
+	printf("HHH\n"); /* XXX */
+	printf("[E] Pointers: [pacA]%p [pacB]%p\n", (void *)pacA, (void *)pacB); /* XXX */
+	printf("[C] pktID='%ld', Time='%ld', Sadd='%s', Sport='%d', Dadd='%s', Dport='%d', length='%d', weight='%d'\n", pacA->pktID, pacA->Time, pacA->Sadd, pacA->Sport, pacA->Dadd, pacA->Dport, pacA->length, pacA->weight); /* TODO DEBUG XXX DELME XXX XXX */
+	printf("[D] pktID='%ld', Time='%ld', Sadd='%s', Sport='%d', Dadd='%s', Dport='%d', length='%d', weight='%d'\n", pacB->pktID, pacB->Time, pacB->Sadd, pacB->Sport, pacB->Dadd, pacB->Dport, pacB->length, pacB->weight); /* TODO DEBUG XXX DELME XXX XXX */
+	if ((strncmp(pacA->Sadd,pacB->Sadd,15))&&
+	    (strncmp(pacA->Dadd,pacB->Dadd,15))&&
+	    (pacA->Sport == pacB->Sport)&&
+	    (pacA->Dport == pacB->Dport)) {
+	    	printf("III_1\n"); /* XXX */
+	    	printf("~~~END same_flow~~~\n"); /* XXX */
 		return 1;
 	}
+	printf("III_0\n"); /* XXX */
+	printf("~~~END same_flow~~~\n"); /* XXX */
 	return 0;
 }
-/* int enqueue(packet pk) { }
+/* int enqueue(packet* pk) { }
  * 
  * Receive packet
  * Add the packet to our data structure
  * Return 0 on success and 1 in case of an error
  */
-int enqueue(packet pk) {
+int enqueue(packet* pk) {
 	/* Function variables */
 	packet* search_head = NULL; /* Pointer to the current element in our round double linked list */
 	/* Init the new packet */
-	packet new_pk = malloc(sizeof(struct Packets));
-	copy_packet(&pk,&new_pk);
+	packet* new_pk = (packet*)malloc(sizeof(packet));
+	new_pk->Time = 0;
+	if (new_pk == NULL) {
+		printf("malloc error\n");
+	}
+	printf("~~~START enqueue~~~\n"); /* XXX */
+	if (STRUCTURE.head != NULL) {printf("[G4] Pointers: [head]%p = %ld\n", (void *)(STRUCTURE.head), (*STRUCTURE.head)->pktID);} /* XXX */
+	if (STRUCTURE.head) {printf("[G6] Pointers: [head]%p=>%p\n", (void *)(STRUCTURE.head), (void *)((*STRUCTURE.head)->next));} /* XXX */
+	printf("[B_0] pktID='%ld', Time='%ld', Sadd='%s', Sport='%d', Dadd='%s', Dport='%d', length='%d', weight='%d'\n", pk->pktID, pk->Time, pk->Sadd, pk->Sport, pk->Dadd, pk->Dport, pk->length, pk->weight); /* TODO DEBUG XXX DELME XXX XXX */
+	printf("[B_1] pktID='%ld', Time='%ld', Sadd='%s', Sport='%d', Dadd='%s', Dport='%d', length='%d', weight='%d'\n", new_pk->pktID, new_pk->Time, new_pk->Sadd, new_pk->Sport, new_pk->Dadd, new_pk->Dport, new_pk->length, new_pk->weight); /* TODO DEBUG XXX DELME XXX XXX */
+	copy_packet(pk,new_pk);
 	printf("[B] pktID='%ld', Time='%ld', Sadd='%s', Sport='%d', Dadd='%s', Dport='%d', length='%d', weight='%d'\n", new_pk->pktID, new_pk->Time, new_pk->Sadd, new_pk->Sport, new_pk->Dadd, new_pk->Dport, new_pk->length, new_pk->weight); /* TODO DEBUG XXX DELME XXX XXX */
-	if (STRUCTURE->head == NULL) { /* This is the first packet in our data structure */
-		STRUCTURE->head = &new_pk; /* The new packet is our new head */
+	if (STRUCTURE.head) {printf("[F] Pointers: [head]%p=>%p\n", (void *)(STRUCTURE.head), (void *)((*STRUCTURE.head)->next));} /* XXX */
+	if (STRUCTURE.head == NULL) { /* This is the first packet in our data structure */
+		STRUCTURE.head = &new_pk; /* The new packet is our new head */
 		new_pk->prev = &new_pk; /* And he his the prev and next of himself */
 		new_pk->next = &new_pk;
-		printf("Pointers: [new_pk]%p=>%p [head]%p=>%p\n", (void *)&new_pk, (void *)(new_pk->next), (void *)(STRUCTURE->head), (void *)((*(STRUCTURE->head))->next)); /* XXX */
-		STRUCTURE->count = 1; /* We only have one packet in our data structure */
+		printf("[C] Pointers: [new_pk]%p=>%p [head]%p=>%p\n", (void *)new_pk, (void *)(new_pk->next), (void *)(STRUCTURE.head), (void *)((*STRUCTURE.head)->next)); /* XXX */
+		STRUCTURE.count = 1; /* We only have one packet in our data structure */
 	} else {
 		/* Search if the packet belong to an old flow */
-		search_head = STRUCTURE->head;
+		search_head = *(STRUCTURE.head);
 		do {
-			if (same_flow(search_head,&new_pk)) { /* If we found a flow that the new packet belongs to */
+			printf("GGG\n"); /* XXX */
+			printf("[D] Pointers: [new_pk]%p=>%p [head]%p=>%p\n", (void *)new_pk, (void *)(new_pk->next), (void *)(STRUCTURE.head), (void *)((*STRUCTURE.head)->next)); /* XXX */
+			if (same_flow(search_head,new_pk)) { /* If we found a flow that the new packet belongs to */
+				printf("JJJ\n"); /* XXX */
 				/* Update the packet before and after to point on the new packet */
-				(*(*search_head)->prev)->next = &new_pk;
-				(*(*search_head)->next)->prev = &new_pk;
+				(*search_head->prev)->next = &new_pk;
+				(*search_head->next)->prev = &new_pk;
 				/* Connect the new packet to the packets before and after */
-				new_pk->next = (*search_head)->next;
-				new_pk->prev = (*search_head)->prev;
+				new_pk->next = search_head->next;
+				new_pk->prev = search_head->prev;
 				/* Disconect the old packet */
-				(*search_head)->next = NULL;
-				(*search_head)->prev = NULL;
+				search_head->next = NULL;
+				search_head->prev = NULL;
 				/* Connect the old and the new packets with the up/down pointers */
-				new_pk->down = search_head;
-				(*search_head)->up = &new_pk;
+				new_pk->down = &search_head;
+				search_head->up = &new_pk;
 				/* Finish */
-				STRUCTURE->count++; /* We added one packet to the data structure */
+				STRUCTURE.count++; /* We added one packet to the data structure */
+				printf("~~~END enqueue~~~\n"); /* XXX */
 				return 0;
 			}
-			search_head = (*search_head)->next; /* Move our search head to the next element */
-		} while (search_head != STRUCTURE->head); /* Until we complets a single round over the list */
+			search_head = *(search_head->next); /* Move our search head to the next element */
+		} while (search_head != *(STRUCTURE.head)); /* Until we complets a single round over the list */
 		/* Havn't found a flow that the new packet belongs to => Create a new one */
-		new_pk->next = STRUCTURE->head;
-		new_pk->prev = (*STRUCTURE->head)->prev;
-		(*(*STRUCTURE->head)->prev)->next = &new_pk;
-		(*STRUCTURE->head)->prev = &new_pk;
+		new_pk->next = STRUCTURE.head;
+		new_pk->prev = (*STRUCTURE.head)->prev;
+		(*(*STRUCTURE.head)->prev)->next = &new_pk;
+		(*STRUCTURE.head)->prev = &new_pk;
 		/* Finish */
-		STRUCTURE->count++; /* We added one packet to the data structure */
+		STRUCTURE.count++; /* We added one packet to the data structure */
 	}
+	printf("~~~END enqueue~~~\n"); /* XXX */
 	return 0;
 }
 
@@ -342,15 +368,15 @@ int enqueue(packet pk) {
  * ??? XXX ??? XXX
  * Return ??? XXX ??? XXX
  */
-int dequeue(packet pk, packet* head_of_line) { /* TODO TODO TODO Remove packet from our data structure */
-	if((pk->next!=NULL)&&(pk->prev!=NULL)){
-	(*pk->next)->prev = pk->prev;
-	(*pk->prev)->next = pk->next;
+int dequeue(packet* pk, packet* head_of_line) { /* TODO TODO TODO Remove packet from our data structure */
+	if ((pk->next!=NULL)&&(pk->prev!=NULL)) {
+		(*pk->next)->prev = pk->prev;
+		(*pk->prev)->next = pk->next;
 	}
-	if (*head_of_line == pk) {
-		*head_of_line = NULL;
+	if (head_of_line == pk) {
+		*head_of_line = NILL;
 	}
-	if(pk->up != NULL ){
+	if (pk->up != NULL ) {
 		(*pk->up)->down = NULL;
 	}
 	free(pk);
@@ -362,16 +388,17 @@ int dequeue(packet pk, packet* head_of_line) { /* TODO TODO TODO Remove packet f
  * For debugging and support use only!!!
  */
 void print() {
-	packet* pkt = STRUCTURE->head;
-	printf("Pointers: [pkt]%p=>%p [head]%p=>%p\n", (void *)pkt, (void *)(*pkt)->next, (void *)(STRUCTURE->head), (void *)((*STRUCTURE->head)->next)); /* XXX */
+	packet* pkt = *(STRUCTURE.head);
+	printf("~~~START print~~~\n"); /* XXX */
+	printf("[A] Pointers: [pkt]%p=>%p [head]%p=>%p\n", (void *)pkt, (void *)pkt->next, (void *)(STRUCTURE.head), (void *)((*STRUCTURE.head)->next)); /* XXX */
 	printf("╔════════════════════════════════════════════════════════════════════════╗\n");
-	printf("║ Current time=%-20ld Number of waiting packets=%-10i ║\n", CLOCK, STRUCTURE->count);
+	printf("║ Current time=%-20ld Number of waiting packets=%-10i ║\n", CLOCK, STRUCTURE.count);
 	printf("╠═════════════════════════╤═══════════════════════════╤══════════════════╣\n");
 	do {
-		printf("Pointers: [pkt]%p=>%p [head]%p=>%p\n", (void *)pkt, (void *)(*pkt)->next, (void *)(STRUCTURE->head), (void *)((*STRUCTURE->head)->next)); /* XXX */
-		printf("║ ID=%-20ld │ Time=%-20ld │ Sent=%5i/%-5i ║\n", (*pkt)->pktID, (*pkt)->Time, (*pkt)->start_byte, (*pkt)->length);
-		pkt = (*pkt)->next; /* Move to the next element */
-	} while (pkt != STRUCTURE->head); /* Until we complets a single round over the list */
+		printf("[B] Pointers: [pkt]%p=>%p [head]%p=>%p\n", (void *)pkt, (void *)pkt->next, (void *)(STRUCTURE.head), (void *)((*STRUCTURE.head)->next)); /* XXX */
+		printf("║ ID=%-20ld │ Time=%-20ld │ Sent=%5i/%-5i ║\n", pkt->pktID, pkt->Time, pkt->start_byte, pkt->length);
+		pkt = *(pkt->next); /* Move to the next element */
+	} while (pkt != *(STRUCTURE.head)); /* Until we complets a single round over the list */
 	printf("╚═════════════════════════╧═══════════════════════════╧══════════════════╝\n");
 	/*╔══════════════════════════════════╤═══════════════════════╤════════════════╗*/
 	/*║               Col1               │         Col2          │      Col3      ║*/
@@ -380,6 +407,8 @@ void print() {
 	/*║                                  │                       │                ║*/
 	/*║                                  │                       │                ║*/
 	/*╚══════════════════════════════════╧═══════════════════════╧════════════════╝*/
+	if (STRUCTURE.head) {printf("[H] Pointers: [head]%p=>%p\n", (void *)(STRUCTURE.head), (void *)((*STRUCTURE.head)->next));} /* XXX */
+	printf("~~~END print~~~\n"); /* XXX */
 }
 /* int main(int argc, char *argv[]) { }
  * 
@@ -394,7 +423,7 @@ int main(int argc, char *argv[]) {
 	int input_weight = 0;		/* The weight */
 	int res = 0;			/* Temporary variable to store function response */
 	long temp = 0;			/* Temporary variable */
-	packet last_packet = NULL;	/* Temporary variable for the last readed packet from the input file */
+	packet* last_packet;		/* Temporary variable for the last readed packet from the input file */
 	/* Check correct call structure */
 	if (argc != 6) {
 		if (argc < 6) {
@@ -435,10 +464,9 @@ int main(int argc, char *argv[]) {
 		input_quantum = (int)temp; /* This is secure because we alreade validate 'temp' max&min values */
 	}
 	/* Init variables */
-	STRUCTURE = (structure)malloc(sizeof(struct DataStructure)); /* TODO free memory */
-	STRUCTURE->head = NULL;
-	STRUCTURE->count = 0;
-	last_packet = malloc(sizeof(struct Packets)); /* TODO free memory */
+	STRUCTURE.head = NULL;
+	STRUCTURE.count = 0;
+	last_packet = malloc(sizeof(packet)); /* TODO free memory & allocation check */
 	input_quantum = input_weight; /* TODO XXX DELME XXX TODO */
 	input_quantum = input_quantum; /* TODO XXX DELME XXX TODO */
 	/* Weighted Round Robin
@@ -451,12 +479,10 @@ int main(int argc, char *argv[]) {
 	 * 3) A packet is sent only if there is enough credit.
 	 */
 	/* Start the clock :) */
-	while ((last_packet = read_packet(input_weight)) != NULL) { /* while there are more packets in file */ /* TODO we will be needed to add check that we also finish to send everything */
-		printf("F_-4\n"); /* TODO XXX DEBUG DELME XXX */
-		if ((CLOCK < last_packet->Time)&&(STRUCTURE->count == 0)) { /* The new packet is from the future... Adjust the clock */
-			printf("F_-3\n"); /* TODO XXX DEBUG DELME XXX */
-			CLOCK = last_packet->Time;
-		} else if (CLOCK < last_packet->Time) { /* It's time to do some calculations */
+	while (read_packet(last_packet,input_weight)) { /* while there are more packets in file */ /* TODO we will be needed to add check that we also finish to send everything */
+		printf("F_-3\n"); /* TODO XXX DEBUG DELME XXX */
+		if ((CLOCK < last_packet->Time)&&(STRUCTURE.count > 0)) { /* The new packet is from the future */
+			/* It's time to do some calculations */
 			printf("F_-2\n"); /* TODO XXX DEBUG DELME XXX */
 /*			if (send_packet(last_packet)) {*/
 /*				fprintf(stderr, F_ERROR_XXXXXX_FAILED_MSG);*/
@@ -471,6 +497,7 @@ int main(int argc, char *argv[]) {
 			}
 			print(); /* TODO XXX DELME XXX TODO */
 		}
+		CLOCK = last_packet->Time; /* Adjust the clock */
 	}
 	/* Exit */
 	return program_end(EXIT_SUCCESS);
